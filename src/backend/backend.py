2ae48fc4b95d3a34d6c96f9ebbf809f30d381b1d
Muhammad Sheharyar <msheharyarqazi@gmail.com>
2024-02-19 21:55:53 +0500

    remote db deployed

--- src/backend/backend.py
+++ src/backend/backend.py
@@ -1,36 +1,23 @@
 from flask import Flask, render_template, request, jsonify
-from flask_mysqldb import MySQL
 from flask_cors import CORS
 import json
 from pptx import Presentation
 from io import BytesIO
 import threading
-import os
-from flask_socketio import SocketIO
 import pyaudio
 import torch
-import torchaudio
-import requests
-import os
-import numpy as np
 import time
-
-
+import psycopg2
 
 app = Flask(__name__)
-socketio = SocketIO(app)
 cors = CORS(app, origins=["http://localhost:3000"])
 
-# socketio = SocketIO(app, cors_allowed_origins="*")  # Set cors_allowed_origins to "*" or your specific frontend URL
+# Configure POSTGRES
+app.config['POSTGRES_HOST'] = 'ep-shy-wind-a1rxd3hf.ap-southeast-1.aws.neon.tech'
+app.config['POSTGRES_USER'] = 'msheharyarqazi'
+app.config['POSTGRES_PASSWORD'] = '4uv9AnrLePWG'
+app.config['POSTGRES_DB'] = 'intellectslide'
 
-
-# Configure MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '1234'
-app.config['MYSQL_DB'] = 'intellectslide-db'
-
-mysql = MySQL(app)
 
 recording_status = {'status': 'record'} #global variable to update recording status
 frames=[]
@@ -38,6 +25,7 @@ model=0
 decoder=0
 utils=0
 stream=0
+start_time=0
 # Load the model and get utility functions
 model, decoder, utils = torch.hub.load(repo_or_dir='snakers4/silero-models',
                                     model='silero_stt',
@@ -47,41 +35,43 @@ model, decoder, utils = torch.hub.load(repo_or_dir='snakers4/silero-models',
 # Assign utility functions to global variables
 read_batch, split_into_batches, read_audio, prepare_model_input = utils
 
+
 def execute_query(query, values=None):
     try:
-        cur = mysql.connection.cursor()
+        connection = psycopg2.connect(
+        host=app.config['POSTGRES_HOST'],
+        user=app.config['POSTGRES_USER'],
+        password=app.config['POSTGRES_PASSWORD'],
+        dbname=app.config['POSTGRES_DB']
+            )
+        
+        cur = connection.cursor()
+        print 
         if values:
             cur.execute(query, values)
         else:
             cur.execute(query)
+       
 
-        result = cur.fetchall()
-        mysql.connection.commit()
+        # result = cur.fetchall()
+        connection.commit()
         cur.close()
+        connection.close()
+        print("connection closed")
         return "DB Action performed successfully"
     except Exception as e:  
+        print(f'Database connection error: {str(e)}')
         return f'Database connection error: {str(e)}'
-
-# @app.route('/add_user')
-# def add_user():
-#     query1="select * from user"
-#     query2="INSERT INTO user (username, name, password, email) VALUES ('afzoo ', 'shosho', '5677', 'khokho@gmail.com')"
-#     # Try to connect to the database
-#     execute_query(query2)
-#     return json.loads("done")
-
-
-
-def record():
+    
+def start_record():
     global model
     global decoder
     global utils
     global stream
+    global frames
+    stream=0
 
     # Pyaudio recording setup
-    
-    
-
     CHUNK = 1024
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
@@ -97,8 +87,10 @@ def record():
                     frames_per_buffer=CHUNK)
 
     print("* recording")
+    
+    global start_time
+    start_time = time.time()
 
-    global frames
     frames.clear()
     # Start recording
     # for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
@@ -116,16 +108,17 @@ def stop_recording():
     global decoder
     global utils
     global stream
-    p = pyaudio.PyAudio()
-       
-
+    global frames
+    
     print("* done recording")
 
     # Stop recording and close the stream
+    p = pyaudio.PyAudio()
+
     stream.stop_stream()
     stream.close()
     p.terminate()
-    start_time = time.time()
+
     import wave
     CHUNK = 1024
     FORMAT = pyaudio.paInt16
@@ -146,38 +139,49 @@ def stop_recording():
     input = prepare_model_input(read_batch(batches[0]),
                                 device=torch.device('cpu'))
 
-
     WAVE_OUTPUT_FILENAME = "D:/University/FYP/FYP Repo/updated.wav"
-
-
-
     output = model(input)
 
-
     end_time = time.time()
+    transcription=""
     for example in output:
         transcription = decoder(example.cpu())
         print(transcription)
 
-
+    global start_time
     execution_time = end_time - start_time
     print(f"Execution time: {execution_time:.2f} seconds")
     
-    query1 = "INSERT INTO temppresentationslide (transcription) VALUES ( %s);"
+    query1 = "INSERT INTO presentationslide (transcription) VALUES ( %s);"
     value=[]
     value.append(transcription)
     status=execute_query(query1, value)
-    string_data = f'{{"transcription": "{transcription}", "execution_time": {execution_time}, "status": "{status}"}}'
+    string_data = f'{{"transcription": "{transcription}", "execution_time": {execution_time:.2f}, "status": "{status}"}}'
     print("Received JSON data:", string_data)
 
     return json.loads(string_data)
 
 
 
+@app.route('/add_user', methods=['GET'])
+def add_user():
+    query2 = "INSERT INTO \"user\"  VALUES ('afzoo', 'shosho', '5677', 'khokho@gmail.com')"
+
+    # Try to connect to the database
+    execute_query(query2)
+    
+
+    string_data = f'{{"status": "user added"}}'
+
+    return json.loads(string_data)
+
+
 @app.route('/start_recording', methods=['GET'])
-def voice_model():
-    threading.Thread(target=record).start()
-    string_data = f'{{"transcription": "lol"}}'
+def start_model():
+    global recording_status
+    recording_status['status'] = "record"
+    threading.Thread(target=start_record).start()
+    string_data = f'{{"status": "recording started"}}'
     return json.loads(string_data)
 
 
@@ -186,15 +190,6 @@ def stop_model():
     global recording_status
     recording_status['status'] = "stop_recording"
     return stop_recording()
-
-    
-
-
-    
-
-
-
-    
 
 @app.route('/upload_pptx', methods=['POST'])
 def upload_pptx():
@@ -216,18 +211,11 @@ def upload_pptx():
                     value.append(shape.text)
                     print(value)
                     status=execute_query(query1, value)
-            print("\n")
-
-        
+            print("\n")        
         string_data = f'{{"transcription": "done"}}'
-
         return json.loads(string_data)
-
     except Exception as e:
         return {'error': f'An error occurred: {str(e)}'}
-    
-
-
     
 
 
